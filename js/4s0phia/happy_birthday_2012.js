c989513966f6c7718fb1c67c9e999e0ce5fc6c88
Solomon Wu <solomon.wzs@gmail.com>
2012-04-06 01:03:59 +0800

    modified:   css/global.css 	modified:   happy_birthday_2012.html 	modified:   js/4s0phia/happy_birthday_2012.js

--- js/4s0phia/happy_birthday_2012.js
+++ js/4s0phia/happy_birthday_2012.js
@@ -1,8 +1,23 @@
 var screenWidth=0;
 var screenHeight=0;
 var canvas=null;
-var context
+var context=null;
+var body=null;
 
 $(function()
 {
+	canvas=$('#myCanvas');
+	body=$('body');
+
+	canvas.prop('width', parseInt(body.css('width')));
+	canvas.prop('height', parseInt(body.css('height')));
+
+	context=canvas[0].getContext('2d');
+
+	context.beginPath();
+	context.moveTo(100, 100);
+	context.lineTo(200, 200);
+	context.lineTo(100, 200);
+	context.closePath();
+	context.stroke();
 });
